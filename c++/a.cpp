317e52e6b06ab59d9338b21ce9a27bdc8d3a1da0
puku2202 <m1834@s.akashi.ac.jp>
2018-08-16 19:23:48 +0900

    あー人類滅びないかな

---
+++ c++/a.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int i;
+	int n;
+	int a[101];
+	for(i=0;i<=100;i++){
+	a[n]=i*i;
+		cout<<"a["<<i<<"]=="<<a[n]<<endl;
+
+	}
+
+
+
+
+	return 0;
+}
+
---
+++ c++/puku.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int sum(int a,int b){
+nt c;
+	c=a+b;
+
+	return c;
+
+}
+int main(void)
+{
+	int i,j,value;	
+	cin>>i;
+	cin>>j;
+	value=sum(i,j);
+	cout<<value<<endl;
+
+
+	return 0;
+}
+
